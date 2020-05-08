75d1c4c54acbf9edece83e7b723f45c8a33a4754
soldonii <dhs0113@gmail.com>
2020-05-08 19:50:52 +0900

    change .env

---
+++ cypress/integration/auth.spec.js
@@ -0,0 +1,37 @@
+describe('<AuthContainer />', () => {
+  before(() => {
+    cy.clearLocalStorageCache();
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorageCache();
+  });
+
+  it('should procee login.', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('img')
+  });
+});
+
+// describe('<HomeContainer />', () => {
+//     cy.contains('Sign Up').click();
+//     cy.url().should('include', '/auth/signup');
+
+//     cy.get('[name=username]').type('testuser').should('have.value', 'testuser');
+//     cy.get('[name=email]').type('test@gmail.com').should('have.value', 'test@gmail.com');
+//     cy.get('[name=password]').type(123123).should('have.value', '123123');
+//     cy.get('[name=confirmationPassword]').type(123123).should('have.value', '123123');
+
+//     cy.get('[type=submit]').click();
+//     cy.url().should('include', '/auth/login');
+//   });
+
+//   it('should proceed login for test user.', () => {
+//     cy.contains('Login').click();
+//     cy.get('[type=email]').type('test@gmail.com').should('have.value', 'test@gmail.com');
+//     cy.get('[type=password]').type(123123).should('have.value', '123123');
+
+//     cy.get('[type=submit]').click();
+//   });
+// });
